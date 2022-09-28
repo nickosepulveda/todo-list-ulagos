d27bc388c11040c20ad4b2df62a13d1d58b98a88
Nicolas <nicolas.sepulveda@kimenda.cl>
2022-09-28 20:00:29 -0300

    task rendering de js a html

---
+++ index.js
@@ -0,0 +1,43 @@
+const tasks = ["task1", "task2"];
+
+document.addEventListener("DOMContentLoaded", function() {
+    // referencias
+    const form = document.querySelector("form");
+    const listContainer = document.querySelector(".list-container");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+
+        tasks.push(data.task);
+        console.log(tasks)
+
+        let tasksRender = tasks.map((task) => {
+            return (`
+                <div class="item-container">
+                    <label for="">
+                        <input type="checkbox" name="" id="">
+                        ${task}
+                    </label>
+                    <button type="button">-</button>
+                </div>
+            `)
+        })
+        listContainer.innerHTML = tasksRender.join("");
+    })
+
+    let tasksRender = tasks.map((task) => {
+        return (`
+            <div class="item-container">
+                <label for="">
+                    <input type="checkbox" name="" id="">
+                    ${task}
+                </label>
+                <button type="button">-</button>
+            </div>
+        `)
+    })
+    listContainer.innerHTML = tasksRender.join("");
+});
