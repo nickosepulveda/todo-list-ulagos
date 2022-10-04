70d6f67e810d1ba3bee0114b714dff95573b9337
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2022-10-04 17:57:18 -0300

    agregado la funcion submittask

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ const tasks = [{
     isCompleted: false
 }
 ];
+const form = document.querySelector("form");
+const listContainer = document.querySelector(".list-container");
 
 document.addEventListener("DOMContentLoaded", function() {
     // referencias
-    const form = document.querySelector("form");
-    const listContainer = document.querySelector(".list-container");
     taskRender()
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
+    form.addEventListener("submit",submitTask);
+});
+
+function submitTask(event){
+    event.preventDefault();
         
         const formData = new FormData(event.target);
         const data = Object.fromEntries(formData);
@@ -29,25 +32,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
         console.log(tasks)
         taskRender()
-    })
+}
 
-    function taskRender(){
-        let li="";
-        tasks.map((task) => {
-            li += `
-                <div class="item-container">
-                    <label for="">
-                        <input type="checkbox" name="" id="">
-                        ${task.name}
-                    </label>
-                    <input type="image" name="eliminar" class="button_eliminar" src="./static/img/56763.png" alt="text"> 
-                </div>
-            `
-        })
-        listContainer.innerHTML = li;
-        document.getElementById('form').reset();
-        
-    };
+function taskRender(){
+    let li="";
+    tasks.map((task) => {
+        li += `
+            <div class="item-container">
+                <label for="">
+                    <input type="checkbox" name="" id="">
+                    ${task.name}
+                </label>
+                <input type="image" name="eliminar" class="button_eliminar" src="./static/img/56763.png" alt="text"> 
+            </div>
+        `
+    })
+    listContainer.innerHTML = li;
+    document.getElementById('form').reset();
     
-});
+};
 //<button class="button_eliminar" style="background: url(./static/img/56763.png) type="button-eliminar">-</button>
