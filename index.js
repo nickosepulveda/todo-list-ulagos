302d12f168c5f847e8980e3fe62a8daf8e546ac7
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2022-10-04 21:06:06 -0300

    3a y 3b, se mejoro el estilo y se arreglo la lista de tarea a la necesarias

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
-//const tasks = [];
-const tasks = [{
-    id: 1,
-    name: "Task1",
-    isCompleted: false
-},
-{
-    id: 2,
-    name: "Task2",
-    isCompleted: false
-}
-];
-const form = document.querySelector("form");
-const listContainer = document.querySelector(".list-container");
+const tasks = ["task1", "task2"];
 
 document.addEventListener("DOMContentLoaded", function() {
     // referencias
-    taskRender()
-    form.addEventListener("submit",submitTask);
-});
+    const form = document.querySelector("form");
+    const listContainer = document.querySelector(".list-container");
 
-function submitTask(event){
-    event.preventDefault();
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
         
         const formData = new FormData(event.target);
         const data = Object.fromEntries(formData);
 
-        tasks.push({
-            id: tasks.length + 1,
-            name: data.task,
-            isCompleted: false
-        });
+        tasks.push(data.task);
         console.log(tasks)
-        taskRender()
-}
 
-function taskRender(){
-    let li="";
-    tasks.map((task) => {
-        li += `
-            <div class="item-container">
-                <label for="">
+        let tasksRender = tasks.map((task) => {
+            return (`
+                <div class="item-container">
+                    <label for="" style="font-family:verdana" >
                     <input type="checkbox" name="" id="">
-                    ${task.name}
-                </label>
+                        ${task.name}
+                    </label>
+                    <input type="image" name="eliminar" class="button_eliminar" src="./static/img/56763.png" alt="text"> 
+                </div>
+            `)
+        })
+        listContainer.innerHTML = tasksRender.join("");
+    })
+
+    let tasksRender = tasks.map((task) => {
+        return (`
+            <div class="item-container">
+                <label for="" style="font-family:verdana" >
+                    <input type="checkbox" name="" id="">
+                        ${task.name}
+                    </label>
                 <input type="image" name="eliminar" class="button_eliminar" src="./static/img/56763.png" alt="text"> 
             </div>
-        `
+        `)
     })
-    listContainer.innerHTML = li;
-    document.getElementById('form').reset();
-    
-};
-//<button class="button_eliminar" style="background: url(./static/img/56763.png) type="button-eliminar">-</button>
+    listContainer.innerHTML = tasksRender.join("");
+});
