b40e6f62df6b37368463b4341976a4332aa5cb0f
v1nte <ivinte99@gmail.com>
2022-10-05 01:19:47 -0300

    add function 'submitTask'

--- index.js
+++ index.js
@@ -16,8 +16,21 @@ const form = document.querySelector("form");
 const listContainer = document.querySelector(".list-container");
 
 const contentLoaded = function(){
+    let tasksRender = tasks.map((task) => {
+        return (`
+            <div class="item-container">
+                <label for="">
+                    <input type="checkbox" name="" id="">
+                    ${task.value}
+                </label>
+                <button type="button">...</button>
+            </div>
+        `)
+    })
+    listContainer.innerHTML = tasksRender.join("");
+}
 
-    form.addEventListener("submit", function(event) {
+const submitTask = function(event){
         event.preventDefault();
         
         const formData = new FormData(event.target);
@@ -42,21 +55,8 @@ const contentLoaded = function(){
             `)
         })
         listContainer.innerHTML = render.join("");
-        document.getElementById("task-form").reset();
-    })
-
-    let tasksRender = tasks.map((task) => {
-        return (`
-            <div class="item-container">
-                <label for="">
-                    <input type="checkbox" name="" id="">
-                    ${task.value}
-                </label>
-                <button type="button">...</button>
-            </div>
-        `)
-    })
-    listContainer.innerHTML = tasksRender.join("");
+       document.getElementById("task-form").reset();
 }
 
 document.addEventListener("DOMContentLoaded", contentLoaded)
+form.addEventListener("submit",submitTask)
