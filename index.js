02e495e2d4f68db3b5644fae8d0a35e59d80659f
Nicolas <nicolas.sepulveda@kimenda.cl>
2022-10-05 19:42:10 -0300

    fix: Se corrigen errores en index.js

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-const tasks = [ {id:1, name:"task1", isCompleted:"true"}, {id:2, name:"task2", isCompleted:"true"}];
+const tasks = [
+    {
+        id: 1,
+        name: "task1",
+        isCompleted: false
+    }, {
+        id: 2,
+        name: "task2",
+        isCompleted: false
+    }
+];
 
-document.addEventListener("DOMContentLoaded", function() {
-    // referencias
-    const form = document.querySelector("form");
-    const listContainer = document.querySelector(".list-container");
+const form = document.querySelector("form");
+const listContainer = document.querySelector(".list-container");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
-
-        tasks.push(data.task);
-        console.log(tasks)
-
-        let tasksRender = tasks.map((task) => {
-            return (`
-                <div class="item-container">
-                    <label for="">
-                        <input type="checkbox" name="" id="">
-                        ${task.name}
-                    </label>
-                    <button type="button">-</button>
-                </div>
-            `)
-        })
-        listContainer.innerHTML = tasksRender.join("");
-        document.getElementById('myform').reset();
-    })
-
-    let tasksRender = tasks.map((task) => {
+const tasksRender = (tasks) => {
+    let taskHTML = tasks.map((task) => {
         return (`
             <div class="item-container">
                 <label for="">
@@ -40,5 +25,29 @@ document.addEventListener("DOMContentLoaded", function() {
             </div>
         `)
     })
-    listContainer.innerHTML = tasksRender.join("");
-});
+    listContainer.innerHTML = taskHTML.join("");
+}
+
+const submitTask = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    tasks.push({
+        id: tasks.length + 1,
+        name: data.task,
+        isCompleted: false
+    });
+
+    tasksRender(tasks);
+
+    form.reset();
+}
+
+const contentLoaded = () => {
+    tasksRender(tasks);
+}
+
+document.addEventListener("DOMContentLoaded", contentLoaded);
+form.addEventListener("submit", submitTask)
