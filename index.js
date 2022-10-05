a3268cdacd5cd014c8eda1346b05a687fd17c3d0
Psiloung <90428095+Psiloung@users.noreply.github.com>
2022-10-05 18:05:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-const tasks = ["task1", "task2"];
+const tasks = [{
+    id: 1,
+    name: "test1", 
+    isCompleted: false
+},
+{
+    id: 2,
+    value: "test2",
+    isCompleted: false
+}];
 
-document.addEventListener("DOMContentLoaded", function() {
-    // referencias
-    const form = document.querySelector("form");
-    const listContainer = document.querySelector(".list-container");
+const form = document.querySelector("form");
+const listContainer = document.querySelector(".list-container");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
+//i.contentLoaded que sea pasada como argumento a addEventListener de DOMContentLoaded (mover codigo actual dentro).
+//ii.submitTask que sea pasada como argumento a addEventListener de submit (mover codigo actual dentro).
+//iii.tasksRender que realice la generacion del html a inyectar dentro de listContainer.
+
+document.addEventListener("DOMContentLoaded", contentloaded);
+form.addEventListener("submit",submitTask);    
+// referencias
+function contentloaded(){
+    tasksRender();
+};
+function submitTask(event){ 
+    event.preventDefault();
         
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
 
-        tasks.push(data.task);
-        console.log(tasks)
-
-        let tasksRender = tasks.map((task) => {
-            return (`
-                <div class="item-container">
-                    <label for="">
-                        <input type="checkbox" name="" id="">
-                        ${task}
-                    </label>
-                    <button type="button">-</button>
-                </div>
-            `)
-        })
-        listContainer.innerHTML = tasksRender.join("");
-    })
-
+    tasks.push({
+        id: tasks.length+1,
+        name: data.task,
+        isCompleted: false
+    });
+    console.log(tasks)
+    tasksRender();
+};
+const tasksRender = function(){
     let tasksRender = tasks.map((task) => {
         return (`
             <div class="item-container">
@@ -38,6 +47,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 <button type="button">-</button>
             </div>
         `)
-    })
+    });
     listContainer.innerHTML = tasksRender.join("");
-});
+    document.getElementById('form').reset();
+}; 
+    //listContainer.innerHTML = tasksRender.join("");
+    
