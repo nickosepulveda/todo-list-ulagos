c53824c580499ab582f4c30480c664049ffc6ec5
Estebanlv06 <estebangiovanni.lagos@alumnos.ulagos.cl>
2022-10-05 11:07:33 -0300

    Se crearon tres funciones en index.js. contentLoaded, submitTask y tasksRender.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//const tasks = ["task1", "task2"];
-
 const tasks = [{
     id: 1,
     name: "Task",
@@ -11,71 +9,59 @@ const tasks = [{
     isCompleted: false
 }
 ];
-document.addEventListener("DOMContentLoaded", function() {
-    // referencias
-    const form = document.querySelector("form");
-    const listContainer = document.querySelector(".list-container");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
+// referencias
+const form = document.querySelector("form");
+const listContainer = document.querySelector(".list-container");
 
-        tasks.push({
-            id: tasks.length + 1,
-            name: data.task,
-            isCompleted: false
-        });
-        console.log(tasks)
+const contentLoaded = () =>{
+    form.addEventListener("submit", submitTask);
+    tasksRender();
+}
 
-        let tasksRender = tasks.map((task) => {
-            return (`
-                <div class="item-container">
-                    <label for="">
-                        <input type="checkbox" class="check-padding" name="" id="">
-                        ${task.name}
-                    </label>
-                    <div class="align-self-center">
-                    <div class="dropdown menu-dropdown" onclick="this.classList.toggle('active');">
-                        <img class="img-menu" src="./static/img/3puntos.png">
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        
-                        <a class="dropdown-item btn-delete" data-id="" href="#">Eliminar</a>
-                        <a class="dropdown-item btn-delete" data-id="" href="#">Editar</a>
-                        <a class="dropdown-item btn-delete" data-id="" href="#">Otras opciones</a>
-                        
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            `)
-        })
-        listContainer.innerHTML = tasksRender.join("");
-        document.getElementById('task-form').reset();
-    })
+const submitTask = (event) =>{
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    tasks.push({
+        id: tasks.length + 1,
+        name: data.task,
+        isCompleted: false
+    });
+    tasksRender();
+    document.getElementById('task-form').reset();
+}
 
-    let tasksRender = tasks.map((task) => {
-        return (`
-            <div class="item-container">
-                <label for="">
-                    <input type="checkbox" name="" id="">
-                    ${task.name}
-                </label>
-                <div class="align-self-center">
-                <div class="dropdown menu-dropdown" onclick="this.classList.toggle('active');">
-                    <img class="img-menu" src="./static/img/3puntos.png">
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    
-                    <a class="dropdown-item btn-delete" data-id="" href="#">Eliminar</a>
-                    <a class="dropdown-item btn-delete" data-id="" href="#">Editar</a>
-                    <a class="dropdown-item btn-delete" data-id="" href="#">Otras opciones</a>
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-        `)
-    })
-    listContainer.innerHTML = tasksRender.join("");
-});
+const tasksRender = () =>{
+    let html = "";
+    tasks.map((task) => {
+    html +=`
+    <div class="item-container">
+        <label for="">
+            <input type="checkbox" class="check-padding" name="" id="">
+            ${task.name}
+        </label>
+        <div class="align-self-center">
+        <div class="dropdown menu-dropdown" onclick="this.classList.toggle('active');">
+            <img class="img-menu" src="./static/img/3puntos.png">
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+            <a class="dropdown-item btn-delete" data-id="" href="#">Eliminar</a>
+            <a class="dropdown-item btn-delete" data-id="" href="#">Editar</a>
+            <a class="dropdown-item btn-delete" data-id="" href="#">Otras opciones</a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+`;})
+listContainer.innerHTML = html;
+};
+
+document.addEventListener("DOMContentLoaded", contentLoaded);
+
+
+
+
+
+
